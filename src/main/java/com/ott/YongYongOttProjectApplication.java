f0424dff67b2f2f45f2aa6b8f11ef646d926ded9
Minsu <zms1120@naver.com>
2023-11-20 14:08:26 +0900

    기본 셋팅

---
+++ src/main/java/com/ott/YongYongOttProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongYongOttProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YongYongOttProjectApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/ott/YongYongOttProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongYongOttProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
